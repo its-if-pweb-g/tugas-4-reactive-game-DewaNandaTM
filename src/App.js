68c2f537e311acc73a3774bca04a95b2c32e8d6d
Dewa Nanda <dewananda2005@gmail.com>
2024-09-24 12:46:00 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  {"src": "/img/spade.png", matched: false},
+  {"src": "/img/clover.png", matched: false},
+  {"src": "/img/heart.png", matched: false},
+  {"src": "/img/diamond.png", matched: false},
+  {"src": "/img/blackjoker.png", matched: false},
+  {"src": "/img/redjoker.png", matched: false}
+]
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Card Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="cardGrid">
+        {cards.map(card => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched} disabled={disabled}/>
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App;
